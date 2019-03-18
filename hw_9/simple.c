89e3a7b95dfa1ec4781cc5813764885cdde56fe3
bennergarrett <tuf15718@temple.edu>
2019-03-18 11:14:56 -0400

    code update-still broke
    added variables

--- hw_9/simple.c
+++ hw_9/simple.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
- 
+
 int main()
 {
+  int max_items = 3;
+  const char* name1 = "Mary";
+  const char* name2 = "Joe";
+  const char* name3 = "Suasn";
+  //const char** array[max_items];
+  const char** array[] = {*name1, *name2, *name3};
 
-  return 0;
+  for (int i=0; i < max_items; i++)
+  {
 
+
+  }
+
+
+  //fprintf(stdout, "%s\n", array);
+return 0;
 }
