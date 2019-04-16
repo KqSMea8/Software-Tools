08769995f561cd0b418b5d64d641c8b87731a7e9
Garrett <tuf15718@temple.edu>
2019-04-16 18:09:46 -0400

    finished

--- hw_12/myprog.py
+++ hw_12/myprog.py
@@ -13,39 +13,54 @@ import os
 ##import random
 ##
 import random
-
 import time
+
+class shuffle(object):
+    mdb = []
+    contents = []
+    def __init__(self):
+
+        directory = '/data/courses/ece_3822/current/music/'
+        f= open("songlist.txt","w+")
+        for root, dirs, files in os.walk(directory, topdown=False):
+            for name in files:
+                if os.path.splitext(name)[1] == ".mp3":
+                    ##add name
+                    self.mdb.append(name)
+
+                    f.write(name + "\n")
+        f.close() 
+    def shuffler(self):
+        random.shuffle(self.mdb)
+        f= open("shuffle.txt", "w+")
+        for i in self.mdb:
+            f.write(i + "\n")
+        f.close()
+
+    def open_music(self):
+        f= open("shuffle.txt", "r")
+        self.contents = f.read()
+#        self.contents.split("\n")
+        f.close()
+
+    def play(self):
+        for song in self.contents:
+            print "Playing: " + song
+            time.sleep(5)
 
 
 if __name__ == "__main__":
-    ##music database
-    ##
-    mdb = []
-    directory = '/data/courses/ece_3822/current/music/'
+
+    music = shuffle()
+
     parser = argparse.ArgumentParser(description='Description: randomly shuffles music')
-#    parser.add_argument('-ngram', action="store", dest='ngram_l',  type=int, help="Ngram length")
-#    parser.add_argument('-pattern', action="store", dest='word',  type=str, help="Ngram word to search")
-#    parser.add_argument('file', type=argparse.FileType('r'), nargs='*')
-#    variables = parser.parse_args()
     parser.parse_args()
 
-    ##for testing local directory
-    ##
-#    for root, dirs, files in os.walk('.'):
-
-    for root, dirs, files in os.walk(directory, topdown=False):
-        for name in files:
-            if os.path.splitext(name)[1] == ".mp3":
-                    ##prints name and directory
-                    ##
-#                    print os.path.join(root, name)
-                    ##prints mp3 name
-                    ##
-#                    print name
-                    mdb.append(name)
+    music.shuffler()
+    music.open_music()
     ##shuffle database
     ##
-    random.shuffle(mdb)
-    for song in mdb:
-        print "Playing: " + song
-        time.sleep(5)
+    music.play()
+#    for song in music.mdb:
+#        print "Playing: " + song
+#        time.sleep(5)
