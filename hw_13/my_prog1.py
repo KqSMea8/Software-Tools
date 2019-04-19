4f0b9b8bf32ba5cffeeed489346f33d3edc42ae9
Garrett <tuf15718@temple.edu>
2019-04-19 09:15:41 -0400

    progress

--- hw_13/my_prog1.py
+++ hw_13/my_prog1.py
@@ -4,14 +4,12 @@ import json
 import random
 import string
 import re
-#import pandas as pd
-#from pandas import ExcelFile
 
 ##was getting a weird encoding error, had to add sys below
+##
 import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
-
 
 ## Variables
 ##
@@ -19,8 +17,11 @@ first_name=[]
 last_name=[]
 location=[]
 website=[]
+bignum = 1000000
+col = 0
 
 ## load files
+##
 with open('utility/names/first-names.json',"r") as json_file:  
     data = json.load(json_file)
     for p in data:
@@ -41,14 +42,15 @@ for line in file:
     content = re.findall(r'"(.*?)"', line)
     website.append(content)
 file.close()
+
 ## example skeleton
 ##
-print random.randint(0, 1000000000000)
-print random.choice(first_name)
-print random.choice(string.ascii_uppercase)
-print random.choice(last_name)
-print random.choice(website)
-print random.choice(location)
+#print random.randint(0, bignum)
+#print random.choice(first_name)
+#print random.choice(string.ascii_uppercase)
+#print random.choice(last_name)
+#print random.choice(website)
+#print random.choice(location)
 
 ## Create a workbook and add a worksheet.
 ##
@@ -56,41 +58,44 @@ workbook = xlsxwriter.Workbook('Persons.xlsx')
 worksheet = workbook.add_worksheet()
 worksheet2 = workbook.add_worksheet()
 worksheet3 = workbook.add_worksheet()
-
-#row = 0
-col = 0
+work_name = 'Persons.xlsx'
 
 for row in xrange(0,3):
-    worksheet.write(row, col, str(random.randint(0, 1000000000000)))
+    name = str(random.choice(first_name)) + " " + str(random.choice(string.ascii_uppercase)) + ". " + str(random.choice(last_name))
+#    print name
+    worksheet.write(row, col, random.randint(0, int(bignum)))
     worksheet.write(row, col + 1, str(random.choice(website)))
-    worksheet.write(row, col + 2, str(random.choice(first_name)))
-    worksheet.write(row, col + 3, str(random.choice(string.ascii_uppercase)))
-    worksheet.write(row, col + 4, str(random.choice(last_name)))
-    worksheet.write(row, col + 5, str(random.choice(location)))
+    worksheet.write(row, col + 2, name)
+    worksheet.write(row, col + 3, str(random.choice(location)))
 
 
 for row in xrange(0,101):
-    worksheet2.write(row, col, str(random.randint(0, 1000000000000)))
+    name = str(random.choice(first_name)) + " " + str(random.choice(string.ascii_uppercase)) + ". " + str(random.choice(last_name))
+#    print name
+    worksheet2.write(row, col, random.randint(0, int(bignum)))
     worksheet2.write(row, col + 1, str(random.choice(website)))
-    worksheet2.write(row, col + 2, str(random.choice(first_name)))
-    worksheet2.write(row, col + 3, str(random.choice(string.ascii_uppercase)))
-    worksheet2.write(row, col + 4, str(random.choice(last_name)))
-    worksheet2.write(row, col + 5, str(random.choice(location)))
+    worksheet2.write(row, col + 2, name)
+    worksheet2.write(row, col + 3, str(random.choice(location)))
 
-#    row += 1
+for row in xrange(0,100,001):
+    name = str(random.choice(first_name)) + " " + str(random.choice(string.ascii_uppercase)) + ". " + str(random.choice(last_name))
+#    print name                                                                                                                              
+    worksheet2.write(row, col, random.randint(0, int(bignum)))
+    worksheet2.write(row, col + 1, str(random.choice(website)))
+    worksheet2.write(row, col + 2, name)
+    worksheet2.write(row, col + 3, str(random.choice(location)))
 
 workbook.close()
+
 ## ways to read
 ##
-#df = pd.read_excel('Persons.xlsx', sheetname='Sheet1')
-#print("Column headings:")
-#print(df.columns)
-
-wb = xlrd.open_workbook('Persons.xlsx') 
+wb = xlrd.open_workbook(work_name) 
 sheet = wb.sheet_by_index(0) 
   
-# For row 0 and column 0 
+## For row 0 and column 0 
+##
 sheet.cell_value(0, 0) 
   
-for i in range(sheet.ncols): 
-    print(sheet.cell_value(0, i)) 
+for i in range(sheet.nrows): 
+#    print(sheet.cell_value(0, i)) 
+    print(sheet.row_values(i)) 
